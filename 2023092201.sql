18a9c11012adf98eb55fc87504e3f12ffc60a240
Htet Aung Kyaw <98970244+hanklokyaw@users.noreply.github.com>
2023-09-25 23:14:23 -0700

    use with tw cn and us dataset
    Uploaded and cleaned china, US, and taiwan hts database

--- 2023092201.sql
+++ 2023092201.sql
@@ -5,9 +5,9 @@ CREATE DATABASE CUSTOMS_DATABASE;
 USE CUSTOMS_DATABASE;
 SELECT * FROM china_hts_2_digits;
 SELECT * FROM china_hts_4_digits;
-SELECT * FROM china_hts_6_digits ORDER BY CODES DESC LIMIT 5;
+SELECT * FROM china_hts_6_digits ORDER BY HTS6 DESC LIMIT 5;
 SELECT COUNT(*) FROM china_hts_6_digits;
-SELECT * FROM china_hts_8_digits ORDER BY CODES DESC LIMIT 10;
+SELECT * FROM china_hts_8_digits ORDER BY HTS8 DESC LIMIT 10;
 
 -- drop 6 digits table
 DROP TABLE china_hts_6_digits;
@@ -63,3 +63,155 @@ LEFT JOIN china_hts_4_digits AS d4
 ON uni.CODES_4D = d4.CODES
 LEFT JOIN china_hts_2_digits AS d2
 ON uni.CODES_2D = d2.CODES;
+
+-- check imported us export raw table
+SELECT * FROM us_export_concordance_2023 ORDER BY hts10 DESC LIMIT 10;
+
+-- create us key table
+CREATE TABLE us_hts_digits_key
+(`CODES_2D` INT NOT NULL,
+`CODES_4D` INT NOT NULL,
+`CODES_6D` BIGINT NOT NULL,
+`CODES_8D` BIGINT NOT NULL,
+`CODES_10D` BIGINT NOT NULL);
+
+-- I just realized that 2 digits, 4 digits, ... hts code should simply name by hts2, hts4, ...
+-- So, I am changing the china hts codes again here.
+
+-- change column name in china_hts_2_digits table
+ALTER TABLE china_hts_2_digits
+CHANGE CODES HTS2 INT;
+ALTER TABLE china_hts_4_digits
+CHANGE CODES HTS4 INT;
+ALTER TABLE china_hts_6_digits
+CHANGE CODES HTS6 BIGINT;
+ALTER TABLE china_hts_8_digits
+CHANGE CODES HTS8 BIGINT;
+ALTER TABLE china_hts_digits_key
+CHANGE CODES_2D HTS2 INT,
+CHANGE CODES_4D HTS4 INT,
+CHANGE CODES_6D HTS6 BIGINT,
+CHANGE CODES_8D HTS8 BIGINT;
+
+DROP TABLE us_hts_6_digits;
+
+-- create 6 digits table
+CREATE TABLE us_hts_6_digits
+(`HTS6` BIGINT NOT NULL,
+`DESCRIPTION` VARCHAR(100),
+`DESCRIPTION_DETAIL` VARCHAR(200));
+
+-- create 8 digits table
+CREATE TABLE us_hts_8_digits
+(`HTS8` BIGINT NOT NULL,
+`DESCRIPTION` VARCHAR(100),
+`DESCRIPTION_DETAIL` VARCHAR(200));
+
+-- create 10 digits table
+CREATE TABLE us_hts_10_digits
+(`HTS10` BIGINT NOT NULL,
+`DESCRIPTION` VARCHAR(100),
+`DESCRIPTION_DETAIL` VARCHAR(200));
+
+-- create all digits table
+CREATE TABLE us_hts_all_digits
+(`HTS2` INT NOT NULL,
+`HTS4` INT NOT NULL,
+`HTS6` BIGINT NOT NULL,
+`HTS8` BIGINT NOT NULL,
+`HTS10` BIGINT NOT NULL,
+`DESCRIPTION` VARCHAR(100),
+`DESCRIPTION_DETAIL` VARCHAR(200));
+
+-- add a temp column to us_hts_6_digits table
+ALTER TABLE us_hts_6_digits
+ADD COLUMN HTS_temp BIGINT;
+ALTER TABLE us_hts_6_digits
+MODIFY COLUMN DESCRIPTION VARCHAR(100),
+MODIFY COLUMN DESCRIPTION_DETAIL VARCHAR(200);
+
+-- insert into us 6 digits table
+INSERT INTO us_hts_6_digits(HTS6, `DESCRIPTION`, DESCRIPTION_DETAIL)
+SELECT LEFT(hts10,6), description_short, description_long
+FROM us_export_concordance_2023; 
+
+-- insert into us 8 digits table
+INSERT INTO us_hts_8_digits(HTS8, `DESCRIPTION`, DESCRIPTION_DETAIL)
+SELECT LEFT(hts10,8), description_short, description_long
+FROM us_export_concordance_2023; 
+
+-- insert into us 10 digits table
+INSERT INTO us_hts_10_digits(HTS10, `DESCRIPTION`, DESCRIPTION_DETAIL)
+SELECT hts10, description_short, description_long
+FROM us_export_concordance_2023; 
+
+-- insert into us 8 digits table
+INSERT INTO us_hts_all_digits(HTS2, HTS4, HTS6, HTS8, HTS10, `DESCRIPTION`, DESCRIPTION_DETAIL)
+SELECT LEFT(hts10,2), LEFT(hts10,4), LEFT(hts10,6), LEFT(hts10,8), hts10, description_short, description_long
+FROM us_export_concordance_2023; 
+
+SELECT us.HTS6, us.DESCRIPTION, us.DESCRIPTION_DETAIL, cn.HTS6, cn.DESCRIPTION
+FROM us_hts_all_digits AS us
+LEFT JOIN china_hts_6_digits AS cn
+ON us.HTS6 = cn.HTS6
+WHERE us.HTS6 = 850440;
+
+-- check taiwan hts database
+USE customs_database;
+SELECT * 
+FROM tw_hts_original
+ORDER BY HTS10;
+
+DROP TABLE tw_hts_original;
+
+-- rename column name
+ALTER TABLE tw_hts_original
+CHANGE `ï»¿CODES` `HTS10` BIGINT;
+
+-- replace all percent and dollar words into symbols
+UPDATE tw_hts_original
+SET UNIT_1_TAX = REPLACE(UNIT_1_TAX, ' PERCENT', "%"), 
+	UNIT_2_TAX = REPLACE(UNIT_2_TAX, ' PERCENT', "%"),
+    UNIT_3_TAX = REPLACE(UNIT_3_TAX, ' PERCENT', "%"),
+    UNIT_1_TAX = REPLACE(UNIT_1_TAX, ' DOLLAR', "$"),
+    UNIT_2_TAX = REPLACE(UNIT_2_TAX, ' DOLLAR', "$"),
+    UNIT_3_TAX = REPLACE(UNIT_3_TAX, ' DOLLAR', "$")
+WHERE UNIT_1_TAX LIKE '% PERCENT' OR
+	  UNIT_2_TAX LIKE '% PERCENT' OR
+      UNIT_3_TAX LIKE '% PERCENT' OR
+      UNIT_1_TAX LIKE '% DOLLAR' OR
+      UNIT_2_TAX LIKE '% DOLLAR' OR
+      UNIT_3_TAX LIKE '% DOLLAR';
+
+-- Duplicate taiwan hts original table
+CREATE TABLE tw_hts_all_digits SELECT * FROM tw_hts_original;
+
+-- add 4 new columns to all digits table
+ALTER TABLE tw_hts_all_digits
+ADD HTS2 INT NOT NULL,
+ADD	HTS4 INT NOT NULL,
+ADD HTS6 BIGINT NOT NULL,
+ADD HTS8 BIGINT NOT NULL;
+
+-- append data into newly created 4 columns
+-- note these are to insert as new values
+INSERT INTO tw_hts_all_digits(HTS2, HTS4, HTS6, HTS8)
+SELECT LEFT(HTS10, 2), LEFT(HTS10, 4), LEFT(HTS10, 6), LEFT(HTS10, 8) 
+FROM tw_hts_all_digits;
+
+-- for updating the existing ones use the following
+UPDATE tw_hts_all_digits
+SET HTS2 = COALESCE(LEFT(HTS10,2), ''),
+	HTS4 = COALESCE(LEFT(HTS10,4), ''),
+    HTS6 = COALESCE(LEFT(HTS10,6), ''),
+    HTS8 = COALESCE(LEFT(HTS10,8), '');
+
+-- check all the columns in taiwan hts all digits table
+SELECT * FROM tw_hts_all_digits;
+SELECT DISTINCT(LEFT(HTS10,2)) FROM tw_hts_all_digits ORDER BY HTS10 DESC LIMIT 100;
+SELECT HTS10 FROM tw_hts_all_digits
+WHERE HTS10 = NULL;
+-- there is a null value record
+
+-- deleting a null value record from hts10 column
+DELETE FROM tw_hts_all_digits WHERE HTS10 IS NULL;
